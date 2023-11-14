3a76a5b849d78b4f2ac8c9b8431c2fbdd191b3f7
JawadAhbab <jawad.ahbab.turna@gmail.com>
2023-11-14 13:18:16 +0600

    🎉 1.4.11

--- dist/types/src/core/UndoStack.d.ts
+++ dist/types/src/core/UndoStack.d.ts
@@ -1,13 +1,13 @@
 import { Undo } from './Undo';
 import { Func } from 'tn-typescript';
 export declare const undo: Undo;
-type Action = 'undo' | 'redo';
+export type UndoStackChangeAction = 'undo' | 'redo';
 interface Methods<T> {
     timeout?: number;
     maxdistance?: number;
     value: () => T | Promise<T>;
     namespace: () => string;
-    onChange: (value: T, prevalue: T, action: Action) => void;
+    onChange: (value: T, prevalue: T, action: UndoStackChangeAction) => void;
 }
 export declare class UndoStack<T> {
     private section;
@@ -17,7 +17,7 @@ export declare class UndoStack<T> {
     constructor(section?: string, methods?: Methods<T>);
     private get enabled();
     private get ns();
-    change(value: any, prevalue: any, action: Action): Promise<void>;
+    change(value: any, prevalue: any, action: UndoStackChangeAction): Promise<void>;
     undo(): Promise<void>;
     redo(): Promise<void>;
     serial(): Promise<number>;
